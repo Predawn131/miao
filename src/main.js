73bce2c7a762069edc75d9be28485d5e35a62c31
Predawn131 <1319114007@qq.com>
2021-03-04 20:07:46 +0800

    第二次

--- src/main.js
+++ src/main.js
@@ -3,6 +3,16 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
+
+import MintUI from 'mint-ui'
+import 'mint-ui/lib/style.css'
+import axios from 'axios'
+Vue.prototype.axios = axios
+Vue.use(ElementUI)
+Vue.use(MintUI)
+
 Vue.config.productionTip = false
 
 new Vue({
